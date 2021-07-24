f6d859b24f56d025a3406cbb3140cf5e17ca49df
Jason <asdewq45445@gmail.com>
2021-07-24 11:17:21 +0800

    fix ip

--- backend/handleapi.py
+++ backend/handleapi.py
@@ -5,14 +5,19 @@ import mobileNkust
 
 app = Flask(__name__)
 CORS(app)
+temp_ = {}
 @app.route("/getcourseinformation", methods=['POST'])
 def getcourseinformation():
+    global temp_
     cookie = request.form.get("data")
+    if cookie in temp_:
+        return jsonify(temp_[cookie])
     cookie = base64.b64decode(cookie).decode('utf8').replace('&',';')
 
     #print(cookie)
     user = mobileNkust.NKUST(cookie)
     returnData = user.returnclassificationCourses()
+    temp_[cookie] = returnData
     return jsonify(returnData)
 
 
